93a224859f3673adb5c76f7e3f6f746d6d8e8676
Ross Leitch <ross@hexhive.io>
2025-01-10 16:08:34 +1300

    Update

--- packages/kinswoman-web/next.config.ts
+++ packages/kinswoman-web/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: '/Kinswomen/'
+  basePath: '/Kinswomen'
 };
 
 export default nextConfig;
